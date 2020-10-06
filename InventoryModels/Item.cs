264ad86ebf708c221eda31f767f6efe68523c108
Tarique Memon <tariq_rauf_memon@hotmail.com>
2020-10-06 21:01:16 +0800

    initial commit

---
+++ InventoryModels/Item.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryModels
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
