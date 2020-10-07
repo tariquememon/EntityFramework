d2328f9b4a8d7d19790354c20c7d3eec27be1918
Tarique Memon <tariq_rauf_memon@hotmail.com>
2020-10-07 22:14:34 +0800

    wip

--- InventoryModels/Item.cs
+++ InventoryModels/Item.cs
@@ -8,5 +8,13 @@ namespace InventoryModels
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public int Quantity { get; set; }
+        public string Description { get; set; }
+        public string Notes { get; set; }
+        public bool IsOnSale { get; set; }
+        public DateTime? PurchasedDate { get; set; }
+        public DateTime? SoldDate { get; set; }
+        public decimal? PurchasePrice { get; set; }
+        public decimal? CurrentOrFinalPrice { get; set; }
     }
 }
