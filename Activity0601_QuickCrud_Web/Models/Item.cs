b25c6de30862a28963fe1e9d1b01b1e838409849
Tarique Memon <tariq_rauf_memon@hotmail.com>
2020-10-18 12:34:52 +0800

    added web project

---
+++ Activity0601_QuickCrud_Web/Models/Item.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Activity0601_QuickCrud_Web.Models
+{
+    public class Item
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        public string Name { get; set; }
+        public virtual int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
